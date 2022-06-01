8b3dc00cd9fd8ddcd865bbfb5cec1b4a0f0a17af
juttasorsa <77781927+juttasorsa@users.noreply.github.com>
2022-06-01 20:23:05 +0300

    näkymien vaihtaminen

---
+++ grid.js
@@ -0,0 +1,6 @@
+function setList(){
+    document.getElementById("container").style="display: block; width: 35em;";
+}
+function setGrid(){
+    document.getElementById("container").style="display: grid; grid-template-columns: auto auto auto; grid-gap: 0.5em;";
+}
